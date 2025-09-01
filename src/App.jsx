61b4537a7d547cc7dcede5babb7f80d490d045ce
Roman ijaz <ejazahmed4720@gmail.com>
2025-09-01 12:10:29 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import QR from "./Components/qrcode/QR"
+
 import "./App.css"
-import Modeltest from "./Components/Modal-Popup/Model-test"
+
 import Github from "./Components/githubprofile/Github"
-import '../src/Components/githubprofile/Github.css'
+
 
 function App() {
   
